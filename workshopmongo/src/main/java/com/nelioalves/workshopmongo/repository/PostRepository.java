fb47cdcc03ae99e7783a77df59eaf264b77cf514
arturalexandree <arturalexandredemelo@gmail.com>
2024-03-29 10:56:04 -0300

    Consulta simples com query methods

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.nelioalves.workshopmongo.repository;
 import com.nelioalves.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
